ebd9bb2bcdd398473ad365b6566b4d43f20022db
Charith Munasinghe <mung@zhaw.ch>
2024-04-24 20:16:24 +0200

    added common aliases

--- bashmod_common.sh
+++ bashmod_common.sh
@@ -1,1 +1,7 @@
 # Common bash helpers
+
+############################ Common Aliases ##############################
+alias cc='clear'
+alias lx='ls -F -SX -g -a'
+alias sbash='source ~/.bashrc'
+alias ebash='subl ~/.bashrc'
